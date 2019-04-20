2c10f35e2ac3955f54c6aea6ef2451fe2aee4537
Claudio Castro <claudioluiz.castro@gmail.com>
2019-04-20 14:26:19 -0300

    Create fake input style and props

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
+    <vue-fake-input
+      :length="5"
+      :fontSize="40"
+    />
   </div>
 </template>
 
 <script>
+import VueFakeInput from './components/VueFakeInput.vue';
+
 export default {
   name: 'app',
   components: {
+    VueFakeInput,
   },
 };
 </script>
--- src/components/VueFakeInput.vue
+++ src/components/VueFakeInput.vue
@@ -1,13 +1,78 @@
 <template>
-  <div></div>
+  <div class="fk-input-container">
+    <input
+      type="text"
+      maxlength="1"
+      :style="{
+        fontSize: fkFontSize,
+        borderBottom: fkBorderColor,
+        color: fkFontColor,
+        width: fkWidth,
+      }"
+      :key="index"
+      v-for="(input, index) in length"
+    />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'vue-fake-input',
+
+  props: {
+    length: {
+      type: Number,
+      required: true,
+    },
+    fontSize: {
+      type: Number,
+      required: false,
+    },
+    inputColor: {
+      type: String,
+      required: false,
+    },
+    fontColor: {
+      type: String,
+      required: false,
+    },
+  },
+
+  computed: {
+    fkFontSize() {
+      return this.fontSize ? `${this.fontSize}px` : '22px';
+    },
+
+    fkBorderColor() {
+      const color = this.inputColor ? this.inputColor : '#42b983';
+      return `3px solid ${color}`;
+    },
+
+    fkFontColor() {
+      const color = this.fontColor ? this.fontColor : '#444444';
+      return color;
+    },
+
+    fkWidth() {
+      const width = this.fontSize ? this.fontSize + 8 : 30;
+      return `${width}px`;
+    },
+
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+  .fk-input-container {
+    display: inline-block;
 
+    input {
+      border: none;
+      margin-left: 10px;
+      text-align: center;
+      &:first-child { margin-left: 0; }
+      &:focus { outline: none; }
+    }
+
+  }
 </style>
